9b94e8ccf0f04dedbe6b4d266ce9604d306b4736
waqaarahmed <ahmedwaqar309@gmail.com>
2023-10-28 08:09:46 +0500

    new files

---
+++ Compress_the_String!.py
@@ -0,0 +1,31 @@
+"""
+Compress the String!
+In this task, we would like for you to appreciate the usefulness of the groupby() 
+function of itertools . 
+You are given a string S. Suppose a character 'c' occurs consecutively X times in 
+the string. Replace these consecutive occurrences of the character 'c' with (X, c)
+in the string.
+For a better understanding of the problem, check the explanation. 
+
+Input Format
+A single line of input consisting of the string S.
+
+Output Format
+A single line of output consisting of the modified string.
+
+Constraints
+All the characters of S denote integers between 0 and 9. 
+
+Sample Input
+1222311
+
+Sample Output
+(1, 1) (3, 2) (1, 3) (2, 1)
+"""
+from itertools import groupby
+S = input()
+data = []
+for x, y in groupby(S):
+    i,j=len(list(y)), int(x)
+    data.append((i,j))
+print(*data)
---
+++ defaultdict.py
@@ -0,0 +1,95 @@
+"""The defaultdict tool is a container in the collections class of Python. 
+It's similar to the usual dictionary (dict) container, but the only difference
+is that a defaultdict will have a default value if that key has not been
+set yet. If you didn't use a defaultdict you'd have to check to see if that 
+key exists, and if it doesn't, set it to what you want.
+
+For example:
+from collections import defaultdict
+d = defaultdict(list)
+d['python'].append("awesome")
+d['something-else'].append("not relevant")
+d['python'].append("language")
+for i in d.items():
+    print i
+
+This prints:
+('python', ['awesome', 'language'])
+('something-else', ['not relevant'])
+
+Task:
+In this challenge, you will be given 2 integers, n and m. There are n words, 
+which might repeat, in word group A. There are m words belonging to word group B. 
+For each m words, check whether the word has appeared in group A or not. 
+Print the indices of each occurrence of m in group A. If it does not appear, print -1.
+
+Example
+
+Group A contains 'a', 'b', 'a' Group B contains 'a', 'c'
+
+For the first word in group B, 'a', it appears at positions
+and in group A. The second word, 'c', does not appear in group A, so print
+
+.
+
+Expected output:
+
+1 3
+-1
+
+Input Format
+
+The first line contains integers,
+and separated by a space.
+The next lines contains the words belonging to group .
+The next lines contains the words belonging to group
+
+.
+
+Constraints
+
+
+
+Output Format
+
+Output
+lines.
+The line should contain the -indexed positions of the occurrences of the
+
+word separated by spaces.
+
+Sample Input
+
+STDIN   Function
+-----   --------
+5 2     group A size n = 5, group B size m = 2
+a       group A contains 'a', 'a', 'b', 'a', 'b'
+a
+b
+a
+b
+a       group B contains 'a', 'b'
+b
+
+Sample Output
+
+1 2 4
+3 5
+
+"""
+from collections import defaultdict
+
+n, m = map(int, input().split())
+A= []
+B=[]
+
+for i  in range(n):
+    A.append(input())
+for i in range(m):
+    B.append(input())
+    
+for i in B:
+    if i in A:
+        print(' '.join([str(x+1) for x in range(len(A)) if A[x] == i]))
+    else:
+        print(-1)

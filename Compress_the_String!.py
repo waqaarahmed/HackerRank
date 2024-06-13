56c7f6789d507febce63fbc599764cb3e93b4d1b
Waqar Ahmed <ahmedwaqar309@gmail.com>
2024-06-14 01:46:13 +0500

    Update Compress_the_String!.py

--- Compress_the_String!.py
+++ Compress_the_String!.py
@@ -7,17 +7,22 @@ the string. Replace these consecutive occurrences of the character 'c' with (X, 
 in the string.
 For a better understanding of the problem, check the explanation. 
 
+
 Input Format
 A single line of input consisting of the string S.
+
 
 Output Format
 A single line of output consisting of the modified string.
 
+
 Constraints
 All the characters of S denote integers between 0 and 9. 
 
+
 Sample Input
 1222311
+
 
 Sample Output
 (1, 1) (3, 2) (1, 3) (2, 1)

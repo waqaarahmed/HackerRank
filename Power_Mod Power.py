3d1e9269a26898b233712374d0f4197e87968873
Waqar Ahmed <ahmedwaqar309@gmail.com>
2024-07-08 21:14:14 +0500

    Update Power_Mod Power.py

--- Power_Mod Power.py
+++ Power_Mod Power.py
@@ -8,23 +8,30 @@ or
 It's also possible to calculate a^b mod m.
 >>> pow(a,b,m)  
 This is very helpful in computations where you have to print the resultant % mod.
+
+
 Note: Here, a and b can be floats or negatives, but, if a third argument is present, b cannot be negative.
 Note: Python has a math module that has its own pow(). It takes two arguments and returns a float. It is uncommon to use math.pow().
+
 
 Task
 You are given three integers:a ,b and m. Print two lines.
 On the first line, print the result of pow(a,b). On the second line, print the result of pow(a,b,m).
 
+
 Input Format
 The first line contains a, the second line contains b, and the third line contains m. 
+
 
 Sample Input
 3
 4
 5
 
+
 Sample Output
 81
+
 """
 a, b, c = int(input()), int(input()), int(input())
 print(pow(a,b))

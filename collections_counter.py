155fb6c4fff3f5b5677f0dc9de7d2d51b67e2cd1
Waqar Ahmed <ahmedwaqar309@gmail.com>
2024-07-11 01:11:51 +0500

    Update collections_counter.py

--- collections_counter.py
+++ collections_counter.py
@@ -1,8 +1,10 @@
-"""A counter is a container that stores elements as dictionary keys,
+"""
+Counter
+A counter is a container that stores elements as dictionary keys,
  and their counts are stored as dictionary values.
 
-Sample Code
 
+Sample Code
 >>> from collections import Counter
 >>> 
 >>> myList = [1,1,2,3,4,5,3,2,3,4,2,1,2,3]
@@ -18,33 +20,31 @@ Counter({2: 4, 3: 4, 1: 3, 4: 2, 5: 1})
 >>> print Counter(myList).values()
 [3, 4, 4, 2, 1]
 
-Task
 
+Task
 is a shoe shop owner. His shop has number of shoes.
 He has a list containing the size of each shoe he has in his shop.
 There are number of customers who are willing to pay
-
 amount of money only if they get the shoe of their desired size.
 
-Your task is to compute how much money
+Your task is to compute how much money earned.
 
-earned.
 
 Input Format
-
-The first line contains
-, the number of shoes.
+The first line contains, the number of shoes.
 The second line contains the space separated list of all the shoe 
-sizes in the shop.
-The third line contains , the number of customers.
+sizes in the shop. The third line contains , the number of customers.
 The next lines contain the space separated values of the desired by the 
-customer and
+customer and, the price of the shoe.
 
-, the price of the shoe.
 
 Constraints
+
+
 Output Format:
 Print the amount of money earned by.
+
+
 Sample Input:
 10
 2 3 4 5 6 8 7 6 5 18
@@ -55,15 +55,20 @@ Sample Input:
 4 40
 18 60
 10 50
+
+
 Sample Output:
 200
+
+
 Explanation:
 Customer 1: Purchased size 6 shoe for $55.
 Customer 2: Purchased size 6 shoe for $45.
 Customer 3: Size 6 no longer available, so no purchase.
 Customer 4: Purchased size 4 shoe for $40.
 Customer 5: Purchased size 18 shoe for $60.
-Customer 6: Size 10 not available, so no purchase."""
+Customer 6: Size 10 not available, so no purchase.
+"""
 
 from collections import Counter
 

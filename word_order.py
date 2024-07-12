ce6a1c6a49d3d77a31f85ec9233ba28028cacae2
Waqar Ahmed <ahmedwaqar309@gmail.com>
2024-07-12 05:12:15 +0500

    Update word_order.py

--- word_order.py
+++ word_order.py
@@ -3,7 +3,10 @@ Word Order
 You are given n words. Some words may repeat. For each word, output its number of 
 occurrences. The output order should correspond with the input order of appearance of 
 the word. See the sample input/output for clarification.
+
 Note: Each input line ends with a "\n" character.
+
+
 Input Format
 The first line contains the integer, n.
 The next n lines each contain a word.
@@ -28,7 +31,9 @@ Sample Output
 Explanation
 There are 3 distinct words. Here, "bcdef" appears twice in the input at the first and
 last positions. The other words appear once each. The order of the first appearances 
-are "bcdef", "abcdefg" and "bcde" which corresponds to the output."""
+are "bcdef", "abcdefg" and "bcde" which corresponds to the output.
+
+"""
 from collections import *
 n = int(input())
 dic = OrderedDict()

fcaad44f206eed5866a4db8262c0e82bae68e16d
Waqar Ahmed <ahmedwaqar309@gmail.com>
2024-07-13 20:23:14 +0500

    Update time_delta.py

--- time_delta.py
+++ time_delta.py
@@ -1,9 +1,8 @@
 """
 TIME DELTA
-When users post an update on social media,such as a URL, image, status update etc.
-, other users in their network are able to view this new post on their news feed. 
-Users can also see exactly when the post was published, i.e, how many hours, 
-minutes or seconds ago.
+When users post an update on social media,such as a URL, image, status update etc., other users in their network are able to view this new post on their news feed. 
+Users can also see exactly when the post was published, i.e, how many hours, minutes or seconds ago.
+
 Since sometimes posts are published and viewed in different time zones, 
 this can be confusing. You are given two timestamps of one such post that a
 user can see on his newsfeed in the following format:

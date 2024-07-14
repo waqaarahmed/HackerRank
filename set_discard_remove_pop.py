c0b4a940ccdf50f99feb52e2ee075ec74df4c5b6
Waqar Ahmed <ahmedwaqar309@gmail.com>
2024-07-14 23:29:46 +0500

    Update set_discard_remove_pop.py

--- set_discard_remove_pop.py
+++ set_discard_remove_pop.py
@@ -1,8 +1,7 @@
 """
+
 remove(x)
-This operation removes element x from the set.
-If element x does not exist, it raises a KeyError.
-The .remove(x) operation returns None.
+This operation removes element x from the set. If element x does not exist, it raises a KeyError. The .remove(x) operation returns None.
 
 Example
 >>> s = set([1, 2, 3, 4, 5, 6, 7, 8, 9])
@@ -17,9 +16,7 @@ set([1, 2, 3, 6, 7, 8, 9])
 KeyError: 0
 
 .discard(x)
-This operation also removes element x from the set.
-If element x does not exist, it does not raise a KeyError.
-The .discard(x) operation returns None.
+This operation also removes element x from the set. If element x does not exist, it does not raise a KeyError. The .discard(x) operation returns None.
 
 Example
 >>> s = set([1, 2, 3, 4, 5, 6, 7, 8, 9])
@@ -35,8 +32,7 @@ set([1, 2, 3, 6, 7, 8, 9])
 set([1, 2, 3, 6, 7, 8, 9])
 
 .pop()
-This operation removes and return an arbitrary element from the set.
-If there are no elements to remove, it raises a KeyError.
+This operation removes and return an arbitrary element from the set. If there are no elements to remove, it raises a KeyError.
 
 Example
 >>> s = set([1])
@@ -52,12 +48,9 @@ You have a non-empty set s, and you have to execute  N commands given in N lines
 The commands will be pop, remove and discard.
 
 Input Format
-The first line contains integer n, the number of elements in the set s.
-The second line contains n space separated elements of set s. All of the elements are 
-non-negative integers, less than or equal to 9.
-The third line contains integer N, the number of commands.
-The next N lines contains either pop, remove and/or discard commands followed by 
-their associated value.
+The first line contains integer n, the number of elements in the set s. The second line contains n space separated elements of set s. All of the elements are 
+non-negative integers, less than or equal to 9. The third line contains integer N, the number of commands. The next N lines contains either pop, remove and/or discard 
+commands followed by their associated value.
 
 Output Format
 Print the sum of the elements of set s on a single line.

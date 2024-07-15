b6e220056a1b121b1aa2f3c558714df8194ca17d
Waqar Ahmed <ahmedwaqar309@gmail.com>
2024-07-15 21:01:54 +0500

    Update piling_up!.py

--- piling_up!.py
+++ piling_up!.py
@@ -11,8 +11,7 @@ Example
 blocks=[1,2,3,8,7]
 Result: No
 After choosing the rightmost element, 7, choose the leftmost element, 1. After than, 
-the choices are 2 and 8. These are both larger than the top block of size 1.
-
+the choices are 2 and 8. These are both larger than the top block of size 1. 
 blocks=[1,2,3,8,7]
 Result: Yes
 Choose blocks from right to left in order to successfully stack the blocks.
@@ -24,8 +23,12 @@ The first line of each test case contains n, the number of cubes.
 The second line contains n space separated integers, denoting the sideLengths of 
 each cube in that order. 
 
+
+
 Output Format
 For each test case, output a single line containing either Yes or No.
+
+
 
 Sample Input
 STDIN        Function
@@ -35,6 +38,8 @@ STDIN        Function
 4 3 2 1 3 4  blocks = [4, 3, 2, 1, 3, 4]
 3            blocks[] size n = 3
 1 3 2        blocks = [1, 3, 2]
+
+
 
 Sample Output
 Yes

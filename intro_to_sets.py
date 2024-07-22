21e5149b54c8521c742e092c36bbbc13c393ed96
Waqar Ahmed <ahmedwaqar309@gmail.com>
2024-07-22 22:37:05 +0500

    Update intro_to_sets.py

--- intro_to_sets.py
+++ intro_to_sets.py
@@ -1,6 +1,7 @@
 """
-A set is an unordered collection of elements without duplicate entries.
-When printed, iterated or converted into a sequence, its elements will appear in an arbitrary order.
+sets:
+A set is an unordered collection of elements without duplicate entries. When printed, iterated or converted into a sequence,
+its elements will appear in an arbitrary order.
 
 Example
 >>> print set()
@@ -22,16 +23,13 @@ set([(6, 'r'), (7, 'a'), (3, 'k'), (4, 'e'), (5, 'r'),
 Basically, sets are used for membership testing and eliminating duplicate entries.
 
 Task
-Now, let's use our knowledge of sets and help Mickey.
-Ms. Gabriel Williams is a botany professor at District College.
-One day, she asked her student Mickey to compute the average of all 
-the plants with distinct heights in her greenhouse.
+Now, let's use our knowledge of sets and help Mickey. Ms. Gabriel Williams is a botany professor at District College. One day, she asked her student Mickey to 
+compute the average of all the plants with distinct heights in her greenhouse.
 
 
 Function Description
-Complete the average function in the editor below.
-average has the following parameters:
-    int arr: an array of integers
+Complete the average function in the editor below average has the following parameters:
+        int arr: an array of integers
 Returns
 float: the resulting float value rounded to 3 places after the decimal
 

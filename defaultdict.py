07611f973cf53bf47a8bad8e72044cce9cee5e50
Waqar Ahmed <ahmedwaqar309@gmail.com>
2024-07-25 22:47:20 +0500

    Update defaultdict.py

--- defaultdict.py
+++ defaultdict.py
@@ -1,4 +1,6 @@
-"""The defaultdict tool is a container in the collections class of Python. 
+"""
+defaultdict:
+The defaultdict tool is a container in the collections class of Python. 
 It's similar to the usual dictionary (dict) container, but the only difference
 is that a defaultdict will have a default value if that key has not been
 set yet. If you didn't use a defaultdict you'd have to check to see if that 

2cb6289b5daca8a040011cc8d7bf5369b6550820
Waqar Ahmed <ahmedwaqar309@gmail.com>
2024-07-27 23:30:37 +0500

    Update collections_namedtuple.py

--- collections_namedtuple.py
+++ collections_namedtuple.py
@@ -1,4 +1,5 @@
-"""collections.namedtuple()
+"""
+collections.namedtuple():
 Basically, namedtuples are easy to create, lightweight object types. They turn 
 tuples into convenient containers for simple tasks. With namedtuples, you don’t 
 have to use integer indices for accessing members of a tuple.

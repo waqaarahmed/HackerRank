60582f80476fd5321b3a52b9d80c5e8805588087
Waqar Ahmed <ahmedwaqar309@gmail.com>
2024-07-29 22:56:30 +0500

    Update check_subset.py

--- check_subset.py
+++ check_subset.py
@@ -1,4 +1,6 @@
-"""You are given two sets, A and B.
+"""
+Subsets:
+You are given two sets, A and B.
 Your job is to find whether set A is a subset of set B.
 If set A is subset of set B, print True.
 If set A is not a subset of set B, print False.

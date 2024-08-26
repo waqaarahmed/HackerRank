6376b1b38a2a8d0e8981b0e5663e08966c62c6e8
Waqar Ahmed <ahmedwaqar309@gmail.com>
2024-08-26 23:32:06 +0500

    Update _itertools.py

--- _itertools.py
+++ _itertools.py
@@ -1,11 +1,13 @@
 """
 itertools.combinations_with_replacement(iterable, r):
+
 This tool returns length subsequences of elements from the input iterable allowing 
 individual elements to be repeated more than once.
 Combinations are emitted in lexicographic sorted order. So, if the input 
 iterable is sorted, the combination tuples will be produced in sorted order.
 
 Sample Code
+
 >>> from itertools import combinations_with_replacement
 >>> 
 >>> print list(combinations_with_replacement('12345',2))
@@ -16,20 +18,25 @@ Sample Code
 [(1, 1), (1, 3), (1, 3), (1, 3), (1, 3), (1, 3), (1, 3), (3, 3), (3, 3), (3, 3)]
 
 Task
+
 You are given a string S.
 Your task is to print all possible size k replacement combinations of the string in 
 lexicographic sorted order.
 
 Input Format
+
 A single line containing the string S and integer value k separated by a space.
 
 Output Format
+
 Print the combinations with their replacements of string S on separate lines.
 
 Sample Input
+
 HACK 2
 
 Sample Output
+
 AA
 AC
 AH
@@ -40,6 +47,7 @@ CK
 HH
 HK
 KK
+
 """
 from itertools import combinations_with_replacement
 S, k = input().split()
